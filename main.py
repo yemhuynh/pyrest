9794b47988893fae5ce10a20e247645c78acd277
Yem Huynh <yem.huynh@yahoo.com>
2024-04-18 11:54:38 -0700

    added mock response

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, send_file
 from flask_restful import Resource, Api, reqparse
 import os, io
-
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,13 +15,15 @@ def get_listings_for_event():
     ticket_id = request.args.get('ticket_id')
     if ticket_id is None:
         ticket_id = "deafult-ticket-id"
-
     print("ticket_id=" + str(ticket_id))
 
-    return {
-        "foo" : "bar",
-        "ticket_id" : ticket_id
-    }
+    f = open('mock_listings_response.json')
+    resp = json.load(f)
+
+
+    f.close()
+
+    return resp
 
 
 if __name__ == "__main__":
