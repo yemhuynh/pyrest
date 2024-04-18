d180e0b9825ae100d849a63fd477b9326c4363ac
Yem Huynh <yem.huynh@yahoo.com>
2024-04-18 10:31:11 -0700

    initial api

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request, send_file
+from flask_restful import Resource, Api, reqparse
+import os, io
+
+
+app = Flask(__name__)
+api = Api(app)
+
+# argument parsing
+parser = reqparse.RequestParser()
+# parser.add_argument('q',help='Pass a sentence to analyse')
+
+@app.route('/event', methods=['GET'])
+def get_listings_for_event():
+    ticket_id = request.args.get('ticket_id')
+    print("ticket_id=" + str(ticket_id))
+
+    return {
+        "foo" : "bar"
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
